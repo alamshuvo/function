a9bd9137ecb8de622a3fcf2c222b5227ad352fb7
iftakhar <iftakharalamshuvo11@gmail.com>
2024-02-02 22:19:02 +0600

    task1 added

---
+++ js-function-practice-tasks/task1.js
@@ -0,0 +1,8 @@
+// ### Task-1  
+// Take four parameters. Multiply the four numbers and then return the result 
+function multiplyNumbers(num1,num2,num3,num4) {
+ const result=num1*num2*num3*num4;
+ return result;   
+}
+
+console.log(multiplyNumbers(10,20,30,40));
